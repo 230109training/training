5c58990ba4d65ccec32fcf84f5de8c47ecc11f5d
Bach T <bach_tran@outlook.com>
2023-01-17 16:04:52 -0500

    Re-organize code into different files

---
+++ demos/week_2/dynamo-db-nodejs-demo/add-item-demo.js
@@ -0,0 +1,46 @@
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+    region: "us-east-1", // replace the region value with the region in which
+    // you created your DynamoDB table
+});
+
+// If you notice the code here, there is nowhere that we are actually 
+// "logging in" to AWS. Therefore, our aws-sdk operations are not permitted
+// to actually access any resources in AWS, like retrieving data from a table
+
+// The best way to configure our application to actually "log-in" is by setting
+// environment variables
+
+// An ENVIRONMENT VARIABLE is a value that can be set onto an operating system.
+// Think of an environmment variable as a global variable for our computer
+// Any application running on our computer can access the environment variables
+// The AWS SDK will automatically look for specially named environment variables
+// that contain our AWS credentials
+// 1. AWS_ACCESS_KEY_ID
+// 2. AWS_SECRET_ACCESS_KEY
+// 3. AWS_DEFAULT_REGION
+// We need to set each of these 3 environment variables. For the first 2 environment
+// variables, we want to set our access keys for the IAM user that we created
+// For the third environment variable, we want to set the region that
+// we are using (us-east-1)
+
+const documentClient = new AWS.DynamoDB.DocumentClient(); // Document Client
+
+const params = {
+    TableName: 'grocery_items',
+    Item: {
+        grocery_item_id: 'abc125',
+        name: 't-bone steak',
+        quantity: 1,
+        price: 12.75
+    }
+};
+
+documentClient.put(params, (err) => {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log("Successfully added item");
+    }
+});
---
+++ demos/week_2/dynamo-db-nodejs-demo/delete-item-demo.js
@@ -0,0 +1,43 @@
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+    region: "us-east-1", // replace the region value with the region in which
+    // you created your DynamoDB table
+});
+
+// If you notice the code here, there is nowhere that we are actually 
+// "logging in" to AWS. Therefore, our aws-sdk operations are not permitted
+// to actually access any resources in AWS, like retrieving data from a table
+
+// The best way to configure our application to actually "log-in" is by setting
+// environment variables
+
+// An ENVIRONMENT VARIABLE is a value that can be set onto an operating system.
+// Think of an environmment variable as a global variable for our computer
+// Any application running on our computer can access the environment variables
+// The AWS SDK will automatically look for specially named environment variables
+// that contain our AWS credentials
+// 1. AWS_ACCESS_KEY_ID
+// 2. AWS_SECRET_ACCESS_KEY
+// 3. AWS_DEFAULT_REGION
+// We need to set each of these 3 environment variables. For the first 2 environment
+// variables, we want to set our access keys for the IAM user that we created
+// For the third environment variable, we want to set the region that
+// we are using (us-east-1)
+
+const documentClient = new AWS.DynamoDB.DocumentClient(); // Document Client
+
+const params5 = {
+    TableName: "grocery_items",
+    Key: {
+        grocery_item_id: "abc123"
+    }
+}
+
+documentClient.delete(params5, (err) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Successfully deleted item");
+    }
+})
